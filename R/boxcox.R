3074131c40a9bfe4ac2eeb765828af99ee5cdcd0
Laura Sloboda <laura.sloboda@gmail.com>
2018-11-13 13:30:04 -0800

    interim

--- R/boxcox.R
+++ R/boxcox.R
@@ -1,35 +1,57 @@
 #' Simplified Box-Cox Transformation
 #'
 #' This function applies an estimate of the power transformation factor (lambda) in a deterministic power function to turn non-normal data into normal data.
-#' However, since lamba is user-fed in this function, it may not necessarily result in a normal distribution. A more complex method such as
-#' maximum likelihood estimate (MLE) or Newton-Raphson may be used to estimate the value of lamba required to achieve a Gaussian distribution, however
+#' However, since lamba is user-defined in this function, it may not necessarily result in a normal distribution. A more complex method, such as
+#' maximum likelihood estimate (MLE) or Newton-Raphson, may be used to estimate the value of lamba required to achieve a Gaussian distribution, however
 #' this is beyond the scope of this assignment.
 #'
-#' @param x Vector to raise to some power.
-#' @param lambda Power to raise \code{x} by.
-#' @param delta Amount to shift x by to ensure positive values
-#' @param plot_it Display a plot of \code{x} vs the output? Use logical.
+#' @param x Initial data set.
+#' @param lambda Estimate of power transformation factor to make \code{x} a normal distribution.
+#' @param delta Amount to shift x by to ensure positive values.
+#' @param plot_it Display a plot of \code{x} vs the output. Use logical.
 #'
-#' @return The vector \code{x}, raised to the power of \code{a}.
+#' @return The vector \code{x}, transformed by the Box-Cox transformation.
 boxcox <- function(x, lambda, delta, plot_it) {
     #Test if x >0; if not, add a shift then proceed
-    #If delta not equal zero, x = x + delta
+    if (delta != 0){
+        #Find min(x) -> take absolute value to get difference between min(x) and 0
+        #Add delta to x
+        x <- x + delta
+    }
 
     #Transform y
     #If abs(lambda) > 3; then return error (this is to stop inappropriate use of the function, which is meant for only small transformations)
 
-    #If lambda = 0
-    y <- log(x)
-
-    #Else (lambda not equal 0)
-    y <- ((x ** lamda) - 1.) / lambda
+    #Apply Box-Cox transformation
+    if (lambda == 0){
+        y <- log(x)
+    } else {
+        y <- ((x^lambda) - 1) / lambda
+    }
 
     #Create plot
-    if (plot_it) print(ggplot2::qplot(x, y))
+    if (plot_it) print(
+        ggplot2::qplot(x, y)
+        )
 
     return(y)
 }
 
+#' @rdname boxcox
+#' @export
+
+#Test 1: Check that negative x is corrected to positive values. (I.e. see that the shift value does what is expected)
+#Test 2: Test that lamba = 0  applies the correct function
+#Test 3: Test that lambda != 0 applies the correct function
+#Test 4: Could compare it to the BoxCox function already in MASS
+
+#Test data set: mtcars
+#lm_model <- lm(mpg ~ cyl, data = mtcars)
+#plot(lm_model)
+
+#y <- boxcox(mtcars$cyl, -1, 0, FALSE)
+#boxcox_model <- lm(y ~ cyl, data = mtcars)
+#plot(boxcox_model)
 
 
 #The Box-Cox transformation is used to make a non-normal distribution into a normal distribution, allowing normal-dependent tests (e.g. t-tests) to be conducted on the data.
